ba8d97724d8cb34f556326e1f102c6a67233a2ff
321-bip <79604277+321-bip@users.noreply.github.com>
2021-08-02 21:36:43 -0300

    ttt

--- ex.js
+++ ex.js
@@ -17,67 +17,8 @@ let arraypalavrasorteada = []
 let butao = document.getElementsByTagName("button")
 let pos;
 
- 
-  function create_tag()
-   {  
-
-     for(let i = 0; i < palavra.length; i++)
-        {
-               criatagp = document.createElement('p')
-               criatagp.innerHTML = "-" + "  "
-               palavrasorteada.appendChild(criatagp)
-               criatagp.setAttribute('class','in')
-           let botaostart = document.getElementById("start").disabled = true 
-               botaostart = document.getElementById("start").style.backgroundColor="red"
-        }
-   }
-
-  function sorteo_start()
-    {
-      let dicas = [fruta,legume]
-      let sorteado = []
-      let posdica = Math.floor(Math.random()*10 / 2)
-      let pospalavra = Math.floor(Math.random()*10)
-     
-          if(posdica <= 2)
-            {
-              sorteado.push(dicas[0])
-              dica.innerText = "fruta"
-
-            }else
-              {
-                sorteado.push(dicas[1])
-                dica.innerText = "legume"
-              }
-
-          palavra = sorteado[0][pospalavra]
-
-          create_tag()
-          
-          console.log(pospalavra)
-          console.log(palavra)
-       
-    } 
-
-  function letras(posisao)
-    {
-      pos = posisao
-      letra[pos].style.backgroundColor="blue"
-      letrasselecionadas = letra[pos].value
-      document.getElementsByClassName("alfabeto")[pos].disabled = true
-      jogar()
-      reiniciajogo()
-    }
-
   function jogar()
     {  
-          arraypalavrasorteada = []
-           
-      for(let i = 0; i < palavra.length; i++)
-        {
-          let procuraletra = palavra.substr(i,1)
-              arraypalavrasorteada.push(procuraletra)
-        }
           let indice = []   
           let idx = arraypalavrasorteada.indexOf(letrasselecionadas)
            if(idx != -1)
@@ -108,25 +49,83 @@ let pos;
                 erro() 
               }              
     }  
+    
+  function letras(posisao)
+    {
+      pos = posisao
+      letra[pos].style.backgroundColor="blue"
+      letrasselecionadas = letra[pos].value
+      document.getElementsByClassName("alfabeto")[pos].disabled = true
+      jogar()
+      reiniciajogo()
+    }
+
+    function create_tag()
+    {  
+ 
+      for(let i = 0; i < palavra.length; i++)
+         {
+                criatagp = document.createElement('p')
+                criatagp.innerHTML = "-" + "  "
+                palavrasorteada.appendChild(criatagp)
+                criatagp.setAttribute('class','in')
+           
+         }
+    }
+ 
+  function sorteo_start()
+    {
+       let dicas = [fruta,legume]
+       let sorteado = []
+       let posdica = Math.floor(Math.random()*10 / 2)
+       let pospalavra = Math.floor(Math.random()*10)
+       let botaostart = document.getElementById("start").disabled = true 
+           botaostart = document.getElementById("start").style.backgroundColor="red"
+      
+           if(posdica <= 2)
+             {
+               sorteado.push(dicas[0])
+               dica.innerText = "fruta"
+ 
+             }else
+               {
+                 sorteado.push(dicas[1])
+                 dica.innerText = "legume"
+               }
+ 
+           palavra = sorteado[0][pospalavra]
+       
+            for(let i = 0; i < palavra.length; i++)
+             {
+               let procuraletra = palavra.substr(i,1)
+                   arraypalavrasorteada.push(procuraletra)
+             }
+ 
+          
+           
+           console.log(pospalavra)
+           console.log(palavra)
+           create_tag()
+    } 
    
    function jogoforca()
     {
      let barra = document.getElementById("barra").style.width = vital + "%"
      forca.setAttribute("class","img" + posimg)
     }
-     jogoforca()
+    
 
-   function erro()
-     {
+  function erro()
+    {
        let erros = document.getElementById("erros")
           erros.innerHTML = numerros
-     }
+    }
   
-   function  acerto()
-     {
+  function  acerto()
+    {
        let acertos = document.getElementById("acertos")
           acertos.innerHTML = numacertos 
-     }
+    }
      
   function reiniciajogo()
    {
@@ -158,5 +157,8 @@ let pos;
         } 
         
    }
-     
-     
+ function main()
+   {
+    jogoforca()
+   }   
+   main()   
